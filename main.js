2486eb322d13eec670d72793cd9f296ac4a8daa3
Backonthefarm <115114641+Backonthefarm@users.noreply.github.com>
2023-02-02 15:34:32 -0500

    Add javascript page

---
+++ main.js
@@ -0,0 +1,17 @@
+function game() {
+    // play game
+    // play five rounds
+    // console based
+}
+
+function playRound() {
+    const playerSelection = 
+}
+
+function playerChoice() {
+    //get input from player
+}
+
+function computerChoice() {
+    // get random input from computer
+}
