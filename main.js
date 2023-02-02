8493121298ed98bac2d9343e391e13e6493c4807
Backonthefarm <115114641+Backonthefarm@users.noreply.github.com>
2023-02-02 16:05:51 -0500

    Add function playerChoice js

--- main.js
+++ main.js
@@ -7,14 +7,32 @@ function game() {
 }
 
 function playRound() {
-    const playerSelection = 
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
 }
 
 function playerChoice() {
     //get input from player
+    let input = prompt('type rock, paper, or scissors');
+    while(input == null) 
+    // the above keeps the user from hit cancel button and break the game
+    input = input.toLowerCase
+
+
 }
 
 function computerChoice() {
     // get random input from computer
     return choices[Math.floor(Math.random()*choices.length)]
 }
+
+
+function validateInput (choice) {
+    if (choice.includes(choice)) {
+        return true;
+    } else {
+        return false;
+    }
+    }
+
+game();
