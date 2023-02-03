4b27fd73e99420f0aa89206cae93ffdfa3aaa99d
Backonthefarm <115114641+Backonthefarm@users.noreply.github.com>
2023-02-03 17:08:09 -0500

    Add playRound function

--- app.js
+++ app.js
@@ -5,4 +5,23 @@ const computerPlay = () => {
     return compChoice
 }
 
-console.log(computerPlay())
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'You tied! You both picked rock'
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'You Tied! You both picked paper'
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'You Tied! You both picked scissors'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You Lost! Paper covers rock'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Win! Rock crushes scissors'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win! Paper covers rock'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lost! Scissors cuts paper'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You Win! Scissors cuts paper'  
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lost! Rock crushes scissors'
+}
